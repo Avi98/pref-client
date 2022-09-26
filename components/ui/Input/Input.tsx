81ab274d495a766271e0d57fc5d38d7a3399fe02
Avinash <avinash.kumar@renegadeinsurance.com>
2022-09-27 01:23:25 +0530

    refactor: minor touch ups

--- components/ui/Input/Input.tsx
+++ components/ui/Input/Input.tsx
@@ -6,7 +6,6 @@ import { fontSize, styleToObj } from "./util";
 
 interface IInput extends React.HTMLProps<HTMLInputElement> {
   label?: string;
-  value: string;
   inputSize?: size;
   type?: string;
   inline?: boolean;
@@ -16,39 +15,45 @@ interface IInput extends React.HTMLProps<HTMLInputElement> {
   placeholder: string;
 }
 
-export const BaseInput = ({
-  label,
-  value,
-  hasError,
-  disabled,
-  placeholder,
-  type = "text",
-  inline = true,
-  inputSize = "sm",
-  error = "Invalid Input",
-  ...inputProps
-}: IInput) => {
-  const root = classNames(s.root, {
-    [s.inline]: inline,
-    [s.error]: hasError,
-    [s.disabled]: disabled,
-  });
+export const BaseInput = React.forwardRef(
+  (
+    {
+      label,
+      hasError,
+      disabled = false,
+      placeholder,
+      type = "text",
+      inline = true,
+      inputSize = "sm",
+      error = "Invalid Input",
+      ...inputProps
+    }: IInput,
+    ref: React.Ref<any>
+  ) => {
+    const root = classNames(s.root, {
+      [s.inline]: inline,
+      [s.error]: hasError,
+      [s.disabled]: disabled,
+    });
 
-  const size = fontSize[inputSize];
+    const size = fontSize[inputSize];
 
-  return (
-    <div className={root} id="root_input" style={styleToObj(size)}>
-      {label ? <label>{label}</label> : null}
-      <div className={s.inputWrapper}>
-        <input
-          disabled
-          type={type}
-          value={value}
-          placeholder={placeholder}
-          {...inputProps}
-        />
-        <div className={s.errorWrapper}>{error}</div>
+    return (
+      <div className={root} id="root_input" style={styleToObj(size)}>
+        {label ? <label>{label}</label> : null}
+        <div className={s.inputWrapper}>
+          <input
+            ref={ref}
+            type={type}
+            disabled={disabled}
+            placeholder={placeholder}
+            {...inputProps}
+          />
+          <div className={s.errorWrapper}>{error}</div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
+
+BaseInput.displayName = "BaseInput";
